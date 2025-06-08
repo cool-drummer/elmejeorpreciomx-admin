2fd76ece846f89c96d8fb3d443ceaa5c30abcf11
Ipsilon0 <pacoarza@msn.com>
2025-06-07 20:57:54 -0600

    feat:test

--- auth_mercado_libre/views/views.py
+++ auth_mercado_libre/views/views.py
@@ -6,7 +6,21 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def callback(request):
+    url = 'https://api.mercadolibre.com/oauth/token'
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': '2392720368406147',
+        'client_secret': '',
+        'code': request.GET.get('code'),
+        'redirect_uri': 'TU_REDIRECT_URI',
+        'code_verifier': 'TU_CODE_VERIFIER'
+    }
     print('////////////////////////////////////////////////////////////////////////////////////')
     print(request)
+    print(data)
     print('////////////////////////////////////////////////////////////////////////////////////')
     return Response({"OK": "OK"})
